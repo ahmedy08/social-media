b3fc4f95f1663b714baca9bf8a6e1f2c70e19a4d
dede_dede_ahmet@hotmail.com <Bm2288486>
2022-11-08 00:13:09 +0300

    Added header

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
-<div></div>
+  <MainHeader/>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
+import MainHeader from './components/MainHeader.vue'
 
 export default defineComponent({
   name: 'App',
+  components: {
+    MainHeader
+  },
   setup() {
     return {}
   },
