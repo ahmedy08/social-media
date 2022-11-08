a40f0d2e9eabd8c36b6e7fdef9540bca1e47229a
dede_dede_ahmet@hotmail.com <Bm2288486>
2022-11-08 19:32:29 +0300

    Main header

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,25 @@
+<template>
+<v-app>
+  <v-toolbar class="pt-4 px-0" dark color="black">
+    <v-container fluid style="border: 1px solid black;">
+      <v-row>
+        <v-col></v-col>
+        <v-col>
+          <v-text-field background-color="#ad2314" rounded></v-text-field>
+        </v-col>
+        <v-col></v-col>
+      </v-row>
+    </v-container>
+  </v-toolbar>
+</v-app>
+</template>
+
+<script>
+export default {
+  name: "MainHeader"
+}
+</script>
+
+<style scoped>
+
+</style>
